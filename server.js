8cafa1dc3642d5385a942ab5846f8128cc731ef5
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-09 15:40:03 -0400

    adding even more environmental variables and connection string

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
+      connectionString : process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
       host : process.env.DATABASE_HOST,
       port : process.env.PORT,
       user : process.env.DATABASE_USER,
