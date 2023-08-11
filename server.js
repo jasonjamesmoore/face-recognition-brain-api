67cb0df6bfd9b06f263baaaabfb878dfcd6ba5ef
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-11 12:06:20 -0400

    changing scripts in server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const db = knex({
   client: 'pg',
   connection: {
