b3d69ce80819687aaf7eb2faa608be621c2a3387
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-13 09:22:18 -0400

    changing connection port settings in server.js

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+// Controllers
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : process.env.DATABASE_HOST,
-      port : 5432,
-      user : process.env.DATABASE_USER,
-      password : process.env.DATABASE_PW,
-      database : process.env.DATABASE_DB
-    }
-  });
+  client: 'pg',
+  connection: {
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
+  }
+});
+
+
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
@@ -31,8 +32,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-    console.log('app runnin');
+app.listen(process.env.port || 5432, () => {
+    console.log(`app runnin`);
 });
 
 /* 
