1d6cdeb55a06c29182cbceb3e9a4718bf502f737
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-13 13:26:27 -0400

    missing comma

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: true,
     host : process.env.DATABASE_HOST,
     port : process.env.PORT,
     user : process.env.DATABASE_USER,
