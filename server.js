cdc8e95310525ede2f6126e9cad42a05bd2337c0
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-13 13:50:27 -0400

    trying host: process.env.DATABASE_URL

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    host : process.env.DATABASE_HOST,
-    port : 5432,
+    host : process.env.DATABASE_URL,
     user : process.env.DATABASE_USER,
     password : process.env.DATABASE_PW,
     database : process.env.DATABASE_DB
