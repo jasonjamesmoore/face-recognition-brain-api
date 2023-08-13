0b4f65a3916649c3140613a74b3426fa05e92384
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2023-08-13 10:59:57 -0400

    trying host address to connect

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_HOST,
+    host : dpg-cj67olp7120s7393lp80-a,
     user : process.env.DATABASE_USER,
     password : process.env.DATABASE_PW,
     database : process.env.DATABASE_DB
