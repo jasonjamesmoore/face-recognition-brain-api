fee3596dd5a8a2427b0816a1bb2fe13b3d1b292e
jasonjamesmoore <111584948+jasonjamesmoore@users.noreply.github.com>
2026-01-06 15:43:28 -0500

    updating clarifai api calls

--- controllers/image.js
+++ controllers/image.js
@@ -1,43 +1,31 @@
-const Clarifai = require('clarifai');
+const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = 'e440847d64324e8795c896895d997099';
+    const USER_ID = 'jasonjamesmoore';       
+    const APP_ID = 'final-project';
+    const MODEL_ID = 'face-detection'; 
+    const IMAGE_URL = imageUrl;
 
-const app = new Clarifai.App({
-    apiKey: '2ce4805be9c84909bd3560718ef8c51d' 
-   });
+    const raw = JSON.stringify({
+        "user_app_id": { "user_id": USER_ID, "app_id": APP_ID },
+        "inputs": [ { "data": { "image": { "url": IMAGE_URL }}} ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Accept': 'application/json', 'Authorization': 'Key ' + PAT },
+        body: raw
+    };
+    return requestOptions;
+}
 
 const handleApiCall = (req, res) => {
-    app.models.predict('face-detection', req.body.input)
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(response => response.json())
     .then(data => {
-      res.json(data);
+        res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with API'))
 }
-// const returnClarifaiRequestOptions = (imageUrl) => {
-//     const PAT = 'e440847d64324e8795c896895d997099';
-//     const USER_ID = 'jasonjamesmoore';       
-//     const APP_ID = 'final-project';
-//     const MODEL_ID = 'face-detection'; 
-//     const IMAGE_URL = imageUrl;
-
-//     const raw = JSON.stringify({
-//         "user_app_id": { "user_id": USER_ID, "app_id": APP_ID },
-//         "inputs": [ { "data": { "image": { "url": IMAGE_URL }}} ]
-//     });
-
-//     const requestOptions = {
-//     method: 'POST',
-//     headers: { 'Accept': 'application/json', 'Authorization': 'Key ' + PAT },
-//     body: raw
-//     };
-//     return requestOptions
-// }
-// const handleApiCall = (req, res) => {
-//     fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(this.state.input), req.body.input)
-//     .then(data => {
-//         res.json(data);
-//     })
-//     .catch(err => res.status(400).json('unable to work with API'))
-// }
-
 
 const handleImage = (req, res, db) => {
     const {id} = req.body;
